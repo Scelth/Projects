b814c9f11fbaff0c24d1875ba09883d1e2484984
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-12 19:03:24 +0400

    Add files via upload

---
+++ AdminPanel/ViewModel/MainVM.cs
@@ -0,0 +1,102 @@
+﻿using Admin.Model.Messages;
+using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using Admin.Model.Services;
+
+namespace Admin.ViewModel
+{
+    class MainVM : ViewModelBase
+    {
+        //private ViewModelBase selectedPage;
+        //private readonly IMessenger _messenger;
+        //private readonly INavigateService _navigateService;
+
+        //public MainVM(IMessenger messenger, INavigateService navigateService)
+        //{
+        //    _messenger = messenger;
+        //    _messenger.Register<NavigationMessage>(this, ReceiveMessage);
+        //    _navigateService = navigateService;
+        //}
+
+        //public ViewModelBase SelectedPage
+        //{
+        //    get
+        //    {
+        //        return selectedPage;
+        //    }
+
+        //    set
+        //    {
+        //        if (selectedPage != value)
+        //        {
+        //            Set(ref selectedPage, value);
+        //        }
+        //    }
+        //}
+
+        //public RelayCommand AddButtonCommand
+        //{
+        //    get => new(() =>
+        //    {
+        //        _navigateService.NavigateTo<AddVM>();
+        //    });
+        //}
+
+        //public RelayCommand EditButtonCommand
+        //{
+        //    get => new(() =>
+        //    {
+        //        _navigateService.NavigateTo<EditVM>();
+        //    });
+        //}
+
+        //public RelayCommand DeleteButtonCommand
+        //{
+        //    get => new(() =>
+        //    {
+        //        _navigateService.NavigateTo<DeleteVM>();
+        //    });
+        //}
+
+        //public void ReceiveMessage(NavigationMessage message)
+        //{
+        //    SelectedPage = App.container.GetInstance(message.VMType) as ViewModelBase;
+        //}
+
+        private ViewModelBase selectedPage;
+        private readonly IMessenger _messenger;
+        private readonly INavigateService _navigateService;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => selectedPage;
+            set
+            {
+                Set(ref selectedPage, value); // Функция Set() автоматически вызывает PropertyChanged.
+            }
+        }
+
+        public void ReceiveMessage(NavigationMessage message)
+        {
+            CurrentViewModel = App.container.GetInstance(message.VMType) as ViewModelBase;
+        }
+
+        public MainVM(IMessenger messenger) // constructor
+        {
+            CurrentViewModel = App.container.GetInstance<MenuVM>(); // Ставлю окно по умолчанию 
+
+            _messenger = messenger;
+
+            // Register - регистрация класса на сообщение какого-то типа 
+            // параметр this - говорит о том, кого именно регистрировать.
+            // this - это объект MainViewModel созданный SimpleInjector-ом в App.Container
+            // ReceiveMessage - Функция, которая запускается после принятия сообщения и принимает в параметрах это сообщение
+            _messenger.Register<NavigationMessage>(this, ReceiveMessage);
+        }
+    }
+}
---
+++ AdminPanel/ViewModel/MenuVM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Views;
+using System.Windows.Controls;
+using System.Windows;
+using Admin.Services.Interfaces;
+using Admin.Model.Services;
+
+namespace Admin.ViewModel
+{
+    internal class MenuVM : ViewModelBase
+    {
+        private readonly INavigateService _navigateService;
+        private readonly IProjectManageService _projectManageService;
+        //public string Username { get; set; }
+
+        public MenuVM(INavigateService navigateService, IProjectManageService projectManageService)
+        {
+            _navigateService = navigateService;
+            _projectManageService = projectManageService;
+        }
+
+        public RelayCommand AddButtonCommand
+        {
+            get => new(() =>
+            {
+                _navigateService.NavigateTo<AddVM>();
+            });
+        }
+
+        public RelayCommand EditButtonCommand
+        {
+            get => new(() =>
+            {
+                _navigateService.NavigateTo<EditVM>();
+            });
+        }
+
+        public RelayCommand DeleteButtonCommand
+        {
+            get => new(() =>
+            {
+                _navigateService.NavigateTo<DeleteVM>();
+            });
+        }
+    }
+}
