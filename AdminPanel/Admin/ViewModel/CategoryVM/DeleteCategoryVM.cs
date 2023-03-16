79c883d2877fbbab8ea48b4011731ce1998878c1
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-16 22:14:00 +0400

    Add files via upload

---
+++ AdminPanel/Admin/ViewModel/CategoryVM/DeleteCategoryVM.cs
@@ -0,0 +1,19 @@
+﻿using Admin.Model.Services;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.ViewModel.CategoryVM
+{
+    class DeleteCategoryVM : ViewModelBase
+    {
+        private readonly INavigateService _navigationService;
+        public DeleteCategoryVM(INavigateService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+    }
+}
