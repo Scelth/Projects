732784bb9fee6efbf4c8401fae6b03bb89b41f25
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-20 17:18:25 +0400

    Add files via upload

---
+++ AdminPanel/Admin/Services/Interfaces/IProjectManageService.cs
@@ -0,0 +1,16 @@
+﻿using Admin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Services.Interfaces
+{
+    interface IProjectManageService
+    {
+        void Add(ProjectModel projectModel);
+        ProjectModel GetProject(string name, string releaseDate);
+        bool CheckExists(string name, string releaseDate);
+    }
+}
