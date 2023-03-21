66ba52b7c272021c008fd773efc1ad0e637fba54
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-21 16:40:06 +0400

    Add files via upload

---
+++ AdminPanel/Admin/Model/ProjectModel.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Model
+{
+    public class ProjectModel
+    {
+        //[JsonIgnore]
+        //public Search[] Search { get; set; }
+        public string Name { get; set; }
+        public string Studio { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Poster { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public DateTime ReleseDate { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class Search
+    {
+        public string Name { get; set; }
+    }
+}
