75c2cf53dc864c23b756f51b753e71d777013d75
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-22 18:59:25 +0400

    Add files via upload

---
+++ Admin/Admin/ViewModel/CategoryVM/DeleteCategoryVM.cs
@@ -0,0 +1,81 @@
+﻿using Admin.Messages;
+using Admin.Model;
+using Admin.Model.Services;
+using Admin.Services.Classes;
+using Admin.Services.Interfaces;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Admin.ViewModel.CategoryVM
+{
+    class DeleteCategoryVM : ViewModelBase
+    {
+        private readonly IMessenger _messenger;
+        private readonly ISerializeService _serializeService;
+        //private readonly INavigateService _navigateService;
+        private readonly IDownloadService _downloadService;
+        public Data Data { get; set; } = new();
+        public CategoryModel categoryModel { get; set; } = new();
+
+        private CategoryModel _categorytModel = new();
+        public CategoryModel CategoryModel
+        {
+            get => _categorytModel;
+
+            set
+            {
+                if (_categorytModel != value)
+                {
+                    Set(ref _categorytModel, value);
+                }
+            }
+        }
+
+        public DeleteCategoryVM(/*INavigateService navigateService,*/ ISerializeService serializeService, IMessenger messenger, IDownloadService downloadService)
+        {
+            //_navigateService = navigateService;
+            _messenger = messenger;
+            _serializeService = serializeService;
+            _downloadService = downloadService;
+
+            _messenger.Register<DataMessage>(this, messenger =>
+            {
+                Data.Categories = messenger.Data as ObservableCollection<CategoryModel>;
+            });
+
+            if (File.Exists("Categories.json"))
+            {
+                //StreamReader file = new("Categories.json");
+                string json = _downloadService.DownloadJson("Categories.json");
+                ObservableCollection<CategoryModel> categories = _serializeService.Deserialize<ObservableCollection<CategoryModel>>(json);
+                Data.Categories = new ObservableCollection<CategoryModel>(categories);
+            }
+        }
+
+        public RelayCommand DeleteCommand
+        {
+            get => new(() =>
+            {
+                var categoryToRemove = Data.Categories.FirstOrDefault(x => x.Name == CategoryModel.Name);
+                if (categoryToRemove != null)
+                {
+                    File.Delete($"{CategoryModel.Name}Category.json");
+                    MessageBox.Show($"{CategoryModel.Name} category deleted");
+                    Data.Categories.Remove(categoryToRemove);
+                    string? json = _serializeService.Serialize<Data>(Data.Categories);
+                    File.WriteAllText("Categories.json", json);
+                }
+            });
+        }
+    }
+}
---
+++ Admin/Admin/ViewModel/ProjectVM/InfoProjectVM.cs
@@ -0,0 +1,82 @@
+﻿using Admin.Messages;
+using Admin.Model;
+using Admin.Model.Services;
+using Admin.Services.Interfaces;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Views;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.ViewModel.ProjectVM
+{
+    internal class InfoProjectVM : ViewModelBase
+    {
+        public ProjectModel Project { get; set; } = new();
+
+        private readonly IMessenger _messenger;
+        private readonly INavigateService _navigationService;
+        private readonly IDownloadService _downloadService;
+
+        public InfoProjectVM(IMessenger messenger, INavigateService navigationService, IDownloadService downloadService)
+        {
+            _messenger = messenger;
+            _navigationService = navigationService;
+            _downloadService = downloadService;
+
+            // Register to receive the selected ProjectModel instance
+            _messenger.Register<DataMessage>(this, message =>
+            {
+                Project = message.Data as ProjectModel;
+            });
+        }
+
+        public RelayCommand BackCommand
+        {
+            get => new(() =>
+            {
+                _navigationService.NavigateTo<SearchProjectVM>();
+            });
+        }
+
+        public RelayCommand EditCommand
+        {
+            get => new(() =>
+            {
+                _navigationService.NavigateTo<EditProjectVM>();
+            });
+        }
+
+        public RelayCommand DeleteCommand
+        {
+            get => new(() =>
+            {
+                var json = _downloadService.DownloadJson($"{Project.Category}Category.json");
+                var projects = JsonConvert.DeserializeObject<List<ProjectModel>>(json);
+
+                // Remove the selected project by name
+                var projectToRemove = projects.FirstOrDefault(p => p.Name == Project.Name);
+                if (projectToRemove != null)
+                {
+                    projects.Remove(projectToRemove);
+
+                    // Write the updated projects back to the JSON file
+                    json = JsonConvert.SerializeObject(projects);
+                    File.WriteAllText($"{Project.Category}Category.json", json);
+
+                    // Set the IsUserControlUpdated property to true
+                    //IsUserControlUpdated = true;
+                }
+
+                // Navigate back to the search page
+                _navigationService.NavigateTo<SearchProjectVM>();
+            });
+        }
+    }
+}
