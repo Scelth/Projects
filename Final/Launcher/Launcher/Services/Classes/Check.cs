a4b76c5fd12a41de8038e7024c43b5520671cf0f
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-26 22:23:44 +0400

    Add files via upload

---
+++ Final/Launcher/Launcher/Services/Classes/Check.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher.Services.Classes
+{
+    public static class Check
+    {
+        public static bool IsLoggedIn { set; get; }
+        public static string Log { set; get; }
+    }
+}
