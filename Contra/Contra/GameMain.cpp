a385d69b3e27e613aa786f026010106b457be044
Scelth <magomed.guluyev@gmail.com>
2024-03-26 13:35:42 +0400

    Start project

---
+++ Contra/Contra/GameMain.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+const std::string RESOURCES_PATH = "Resources/";
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(330, 400), "Contra");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return 0;
+}
